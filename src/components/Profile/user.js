0e48e4dfeb639f1e6c4452af82b6bec8b2359827
lgavlich <lgavlich@gmail.com>
2021-09-24 17:47:08 +0300

    user

--- src/components/Profile/user.js
+++ src/components/Profile/user.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import defaultImage from './components/default.jpg';
+
 const Profile = ({ name, tag, location, avatar, stats }) => {
   return (
     <div className="profile">
@@ -25,4 +29,16 @@ const Profile = ({ name, tag, location, avatar, stats }) => {
     </div>
   );
 };
+Profile.defaultProps = {
+  avatar: defaultImage,
+};
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.string.isRequired,
+};
+
 export default Profile;
