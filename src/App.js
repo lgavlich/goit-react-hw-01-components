5d19f24a66d95a6cd1e7d7da5a1ca7ddb859cb43
lgavlich <lgavlich@gmail.com>
2021-09-25 14:12:20 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import Profile from './components/Profile/user';
+import Statistics from './components/statistical/statistics';
 import FriendList from './components/Friend-List/FriendList';
+import TransactionHistory from './components/Transaction/TransactionHistory';
+import statisticalData from './data/statistical-data.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 import user from './data/user.json';
 
 const App = () => {
   return (
-    <Profile
-      name={user.name}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics title="Upload stats" stats={statisticalData} />
+
+      <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
+    </>
   );
 };
 
